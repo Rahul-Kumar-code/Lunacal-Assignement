625bc2ddb293be2aa1b02bfd4f4ef3ecc3041f32
Rahul-Kumar-code <rk0505469@gmail.com>
2025-10-24 10:07:12 +0530

    Fixed layout issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
-// app/layout.js
 import "@fontsource/inter";
-
-import './globals.css';
+import "./globals.css";
+import { ReactNode } from "react";
 
 export const metadata = {
-  title: 'Lunacal Assignment',
-  description: 'Prototype assignment by Rahul',
+  title: "Lunacal Assignment",
+  description: "Prototype assignment by Rahul",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-900 text-gray-100 font-sans">
-        <div className="flex h-screen">
-          {children}
-        </div>
+        <div className="flex h-screen">{children}</div>
       </body>
     </html>
   );
